717b677a11e47dda8528ffc3ddd5f583424416d9
Prashant Solanki <prashantsolanki2025@gmail.com>
2024-06-03 19:48:06 +0530

    Retrieved messages from firestore

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +15,46 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+
+  late User loggedInUser;
+  late String messageText;
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser;
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // void getMessages() async {
+  //   final messages = await _firestore.collection("messages").get();
+  //   for (var message in messages.docs) {
+  //     print(message.data());
+  //   }
+  // }
+
+  // void messageStream() async {
+  //   final stream = _firestore.collection("messages").snapshots();
+  //   await for (var snapshot in stream) {
+  //     for (var message in snapshot.docs) {
+  //       print(message.data());
+  //     }
+  //   }
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +64,8 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
             onPressed: () => {
               // Logout Function
+              _auth.signOut(),
+              Navigator.pop(context),
             },
             icon: Icon(Icons.close),
           )
@@ -29,11 +74,40 @@ class _ChatScreenState extends State<ChatScreen> {
         backgroundColor: Colors.lightBlueAccent,
         centerTitle: true,
       ),
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            StreamBuilder<QuerySnapshot>(
+              stream: _firestore.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                List<Text> messageWidgets = [];
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
+                  );
+                }
+                // '?' null check whether snapshot has data
+                final messages = snapshot.data?.docs;
+                // '!' can't use nullable expression in for-in loop
+                for (var message in messages!) {
+                  final messageText = message.get('text');
+                  final messageSender = message.get('sender');
+                  final messageWidget = Text(
+                    "$messageText from $messageSender",
+                    style: TextStyle(color: Colors.black),
+                  );
+                  messageWidgets.add(messageWidget);
+                }
+                return Column(
+                  children: messageWidgets,
+                );
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -43,6 +117,7 @@ class _ChatScreenState extends State<ChatScreen> {
                     child: TextField(
                       onChanged: (value) => {
                         //   Message Implementation
+                        messageText = value,
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
@@ -50,6 +125,9 @@ class _ChatScreenState extends State<ChatScreen> {
                   TextButton(
                     onPressed: () => {
                       //   Send Implmentaion
+                      //   messageText + user.email
+                      _firestore.collection("messages").add(
+                          {'text': messageText, 'sender': loggedInUser.email}),
                     },
                     child: Text(
                       "Send",
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/components/rounded_button.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../constants.dart';
 
@@ -14,51 +17,81 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  late String email;
+  late String password;
+
+  bool showSpinner = false;
+
+  final _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Hero(
-              tag: 'logo',
-              child: Container(
-                child: Image.asset('images/logo.png'),
-                height: 150.0,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  child: Image.asset('images/logo.png'),
+                  height: 150.0,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) => {
-                // user input
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: "E-Mail"),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            TextField(
-              onChanged: (value) => {
-                // user input
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: "Password"),
-            ),
-            SizedBox(
-              height: 32.0,
-            ),
-            RoundedButton(
-                buttonColor: Colors.lightBlueAccent,
-                onPressed: () => {
-                      //   Log In functionality implementation
-                    },
-                buttonText: "Log In")
-          ],
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) => {
+                  email = value,
+                },
+                decoration: kTextFieldDecoration.copyWith(hintText: "E-Mail"),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) => {
+                  password = value,
+                },
+                decoration: kTextFieldDecoration.copyWith(hintText: "Password"),
+              ),
+              SizedBox(
+                height: 32.0,
+              ),
+              RoundedButton(
+                  buttonColor: Colors.lightBlueAccent,
+                  onPressed: () async {
+                    //   Log In functionality implementation
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    try {
+                      final existingUser =
+                          await _auth.signInWithEmailAndPassword(
+                              email: email, password: password);
+                      if (existingUser != null) {
+                        Navigator.pushNamed(context, ChatScreen.id);
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  },
+                  buttonText: "Log In")
+            ],
+          ),
         ),
       ),
     );
